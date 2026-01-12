c0a7d2a96b27e0a8d3edb534d3c584bd9206a444
publejuble <seohyeon0928@gmail.com>
2026-01-12 16:58:15 +0900

    챕터 3 문제

---
+++ Hello3/MainApp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RectArea
+{
+    class MainApp
+    {
+        public static void Main()
+        {
+            Console.WriteLine("사각형의 너비를 입력하세요.");
+            string width = Console.ReadLine();
+
+            Console.WriteLine("사각형의 높이를 입력하세요.");
+            string height = Console.ReadLine();
+
+            double w = double.Parse(width);
+            double h = double.Parse(height);
+
+            double area = w * h;
+
+            Console.WriteLine($"사각형의 넓이는: {area}입니다.");
+        }
+    }
+}
